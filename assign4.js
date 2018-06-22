7ca1c6d8630e1c0d2879036a72a298a357ec0ce8
Chiman-Dev <bchiman.subudhi@gmail.com>
2018-06-22 08:40:27 +0530

    modified files

--- assign4.js
+++
@@ -1,7 +0,0 @@
-function createObject(name, height) {
-this.name = name;
-this.height = height;
-this.describe = function () {
-return "I am "+this.name + ". " +"I am "+ this.height+"cm height";
-}
-}
